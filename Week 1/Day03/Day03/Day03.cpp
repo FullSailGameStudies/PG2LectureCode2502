9c2301dbcf04ae78ce65c1d9285dfcf03e47b0b1
Garrett Girod <girodg@hotmail.com>
2025-02-07 10:05:52 -0500

    optional arguments

--- Week 1/Day03/Day03/Day03.cpp
+++ Week 1/Day03/Day03/Day03.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include "ThemeSong.h"
 
 std::string postFix(std::string fileName, int postFixNumber = 1) //postFixNumber is optional
 {
@@ -37,13 +38,18 @@ int main()
         CHALLENGE 1:
 
             Write a method called batTheme.
-            Add an optional parameter to determine how many "na" are printed. The default value should be 13.
+            Add an optional parameter to determine how many "na" are printed. 
+            The default value should be 13.
             
-            If the calling code does not pass a value for the parameter, print "na na na na na na na na na na na na na Batman".
+            If the calling code does not pass a value for the parameter, 
+            print "na na na na na na na na na na na na na Batman".
             If a value is passed, print the number of "na" equal to the value.
             EX: if 6 is passed, print "na na na na na na Batman"
 
     */
+    ThemeSong themes;
+    themes.batTheme();
+    themes.batTheme(50);
 
 
 
@@ -63,10 +69,11 @@ int main()
         3) passing a vector to the constructor of another vector
 
     */
-    std::vector<int> scores = { 1,2,3,4,5 };
+    std::vector<int> scores = { 1,2,3,4,5,6,7,8,9,0,11,12,13,14 };
 
     //1) copy each element manually
     std::vector<int> scores2;
+    scores2.reserve(scores.size());
     for (size_t i = 0; i < scores.size(); i++)
         scores2.push_back(scores[i]);
 
@@ -76,6 +83,9 @@ int main()
     //3) passing a vector to the constructor of another vector
     std::vector<int> scores4(scores);
 
+    std::cout << scores2.capacity() << "\n";
+    std::cout << scores3.capacity() << "\n";
+    std::cout << scores4.capacity() << "\n";
 
 
     /*
